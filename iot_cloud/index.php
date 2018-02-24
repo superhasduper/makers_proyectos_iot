8cefd029c520706e0944b49531815c5a1327eb9d
superhasduper <superhasduper@gmail.com>
2018-02-24 11:57:14 +0100

    basic structure setup
    basic structure setup with php, css and javascript files

---
+++ iot_cloud/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>IOT Cloud</title>
+    <link rel="stylesheet" href="styles/style.css">
+   <script src="scripts/script.js"></script>
+   </head>
+<body>
+    <h2>Hola mundo</h2>
+</body>
+</html>
